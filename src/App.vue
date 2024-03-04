ef21162b91cc77bb03ebdeef4b806ad46eab6463
hiep0702 <trantuanhiep@gmail.com>
2024-03-04 11:30:35 +0700

    feature swtch screen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <h1>hi im hie</h1>
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen v-if="statusMatch === 'match'" />
 </template>
 
 <script>
+import InteractScreen from "./components/InteractScreen.vue";
+import MainScreen from "./components/MainScreen.vue";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      statusMatch: "default",
+    };
+  },
+  components: {
+    MainScreen,
+    InteractScreen,
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("run onHandleBeforeStart, ", config);
+      this.statusMatch = "match";
+    },
+  },
 };
 </script>
